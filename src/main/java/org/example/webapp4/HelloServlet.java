0923ff4639d8b4c3c997b50f252587dc596fe3e4
rajatkinlekar <kinlekarrajat@gmail.com>
2024-11-25 00:54:07 +0530

    added +F

--- src/main/java/org/example/webapp4/HelloServlet.java
+++ src/main/java/org/example/webapp4/HelloServlet.java
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-        System.out.println(a+b+c+d+e);
+        System.out.println(a+b+c+d+e+F);
 
         // Hello
         PrintWriter out = response.getWriter();
